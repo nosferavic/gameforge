abd22e867a5a0b70f00f69bfda320cac688f229f
victor viana <victor.deviana@outlook.com>
2024-05-15 21:36:27 -0300

    burguerMenu

--- script/script.js
+++ script/script.js
@@ -1,21 +1,26 @@
-const btnMobile = document.getElementById("btn-mobile");
-
-function toggleMenu(event) {
-  if (event.type === "touchstart") event.preventDefalut();
+function burguerMenu() {
+  const btnMobile = document.getElementById("btn-mobile");
   const nav = document.getElementById("nav");
-  nav.classList.toggle("ativo");
-  const ativo = nav.classList.contains("ativo");
-  event.currentTarget.setAttribute("aria-expanded", ativo);
-  if (ativo) {
-    event.currentTarget.setAttribute("aria-label", "fechar menu");
-  } else {
-    event.currentTarget.setAttribute("aria-label", "abrir menu");
+  function toggleMenu(event) {
+    if (event.type === "touchstart") event.preventDefalut();
+    nav.classList.toggle("ativo");
+    const ativo = nav.classList.contains("ativo");
+    event.currentTarget.setAttribute("aria-expanded", ativo);
+    if (ativo) {
+      event.currentTarget.setAttribute("aria-label", "fechar menu");
+    } else {
+      event.currentTarget.setAttribute("aria-label", "abrir menu");
+    }
   }
+
+  btnMobile.addEventListener("click", toggleMenu);
+  btnMobile.addEventListener("touchstart", toggleMenu);
+
+  const links = Array.from(document.querySelectorAll(".link"));
+  const link = links.forEach((link) => {
+    link.addEventListener("click", () => {
+      nav.classList.remove("ativo");
+    });
+  });
 }
-
-btnMobile.addEventListener("click", toggleMenu);
-btnMobile.addEventListener("touchstart", toggleMenu);
-
-
-
-
+burguerMenu()
